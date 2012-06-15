77eb340201b71643fc072b0d7cda2f42c7ac7463
Richard <othercat@gmail.com>
2012-06-15 13:50:38 +0800

    Version 1.7
    Return to the right version

--- screenresolution/main.m
+++ screenresolution/main.m
@@ -1,7 +1,10 @@
+
+#define VERSION "1.7"
 // vim: ts=4:sw=4
 /*
  * screenresolution sets the screen resolution on Mac computers.
  * Copyright (C) 2011  John Ford <john@johnford.info>
+ * Modified by Li Richard at 2012-06-15
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,12 +21,11 @@
  * 02110-1301, USA.
  */
 
-#include <ApplicationServices/ApplicationServices.h>
-#include <Foundation/Foundation.h>
+#import <ApplicationServices/ApplicationServices.h>
 
 // Number of modes to list per line.
 #define MODES_PER_LINE 3
-#define VERSION "1.6"
+
 // I have written an alternate list routine that spits out WAY more info
 // #define LIST_DEBUG 1
 
@@ -43,69 +45,61 @@ unsigned int listAvailableModes(CGDirectDisplayID display, int displayNum);
 unsigned int parseStringConfig(const char *string, struct config *out);
 size_t bitDepth(CGDisplayModeRef mode);
 
-// http://stackoverflow.com/questions/3060121/core-foundation-equivalent-for-nslog/3062319#3062319
+// http://stackoverflow.com/questions/3060121/core-foundation-equivalent-for-NSLog/3062319#3062319
 //void NSLog(CFStringRef format, ...);
-
 
 
 int main(int argc, const char *argv[]) {
     // http://developer.apple.com/library/IOs/#documentation/CoreFoundation/Conceptual/CFStrings/Articles/MutableStrings.html
-    int i;
-    CFMutableStringRef args = CFStringCreateMutable(NULL, 0);
-    CFStringEncoding encoding = CFStringGetSystemEncoding();
-    CFStringAppend(args, CFSTR("starting screenresolution argv="));
-    for (i = 0 ; i < argc ; i++) {
-        CFStringAppendCString(args, argv[i], encoding);
-        // If I were so motivated, I'd probably use CFStringAppendFormat
-        CFStringAppend(args, CFSTR(" "));
-    }
-    // This has security implications.  Will look at that later
-    NSLog(CFSTR("%@"), args);
     unsigned int exitcode = 0;
-
+    
     if (argc > 1) {
-        int d;
-        int keepgoing = 1;
+        printf("starting screenresolution argv=");
+        for (int i = 1 ; i < argc ; i++) {
+            printf("%s ",argv[i]);
+        }
+        printf("\n");
+   
         CGError rc;
         uint32_t displayCount = 0;
         uint32_t activeDisplayCount = 0;
         CGDirectDisplayID *activeDisplays = NULL;
-
+        
         rc = CGGetActiveDisplayList(0, NULL, &activeDisplayCount);
         if (rc != kCGErrorSuccess) {
-            NSLog(CFSTR("%s"), "Error: failed to get list of active displays");
+            printf("Error: failed to get list of active displays\n");
             return 1;
         }
         // Allocate storage for the next CGGetActiveDisplayList call
         activeDisplays = (CGDirectDisplayID *) malloc(activeDisplayCount * sizeof(CGDirectDisplayID));
         if (activeDisplays == NULL) {
-            NSLog(CFSTR("s"), "Error: could not allocate memory for display list");
+            printf("Error: could not allocate memory for display list\n");
             return 1;
         }
         rc = CGGetActiveDisplayList(activeDisplayCount, activeDisplays, &displayCount);
         if (rc != kCGErrorSuccess) {
-            NSLog(CFSTR("%s"), "Error: failed to get list of active displays");
+            printf("Error: failed to get list of active displays\n");
             return 1;
         }
-
+        
         // This loop should probably be in another function.
-        for (d = 0; d < displayCount && keepgoing; d++) {
+        for (int i = 0; i < displayCount; i++) {
             if (strcmp(argv[1], "get") == 0) {
-                if (!listCurrentMode(activeDisplays[d], d)) {
+                if (!listCurrentMode(activeDisplays[i], i)) {
                     exitcode++;
                 }
             } else if (strcmp(argv[1], "list") == 0) {
-                if (!listAvailableModes(activeDisplays[d], d)) {
+                if (!listAvailableModes(activeDisplays[i], i)) {
                     exitcode++;
                 }
             } else if (strcmp(argv[1], "set") == 0) {
-                if (d < (argc - 2)) {
-                    if (strcmp(argv[d+2], "skip") == 0 && d < (argc - 2)) {
-                        printf("Skipping display %d\n", d);
+                if (i < (argc - 2)) {
+                    if (strcmp(argv[i+2], "skip") == 0 && i < (argc - 2)) {
+                        printf("Skipping display %d\n", i);
                     } else {
                         struct config newConfig;
-                        if (parseStringConfig(argv[d + 2], &newConfig)) {
-                            if (!configureDisplay(activeDisplays[d], &newConfig, d)) {
+                        if (parseStringConfig(argv[i + 2], &newConfig)) {
+                            if (!configureDisplay(activeDisplays[i], &newConfig, i)) {
                                 exitcode++;
                             }
                         } else {
@@ -113,20 +107,49 @@ int main(int argc, const char *argv[]) {
                         }
                     }
                 }
-            } else if (strcmp(argv[1], "-version") == 0) {
+            } else if (strcmp(argv[1], "--help") == 0) {
+                // Send help information to stdout since it was requested
+                printf("screenresolution sets the screen resolution on Mac computers.\n\n");
+                printf("screenresolution version %s Licensed under GPLv2\n", VERSION);
+                printf("Copyright (C) 2011  John Ford <john@johnford.info> Modified by Li Richard at 2012\n\n");
+                printf("usage: screenresolution [get]    - Show the resolution of all active displays\n");
+                printf("       screenresolution [list]   - Show available resolutions of all active displays\n");
+                printf("       screenresolution [skip] [display1resolution] [display2resolution]\n");
+                printf("                                 - Sets display resolution and refresh rate\n");
+                printf("       screenresolution --version - Displays version information for screenresolution\n"); 
+                printf("       screenresolution --help    - Displays this help information\n\n"); 
+                printf("examples: screenresolution 800x600x32            - Sets main display to 800x600x32\n");
+                printf("          screenresolution 800x600x32 800x600x32 - Sets both displays to 800x600x32\n");
+                printf("          screenresolution skip 800x600x32       - Sets second display to 800x600x32\n\n");
+            } else if (strcmp(argv[1], "--version") == 0) {
                 printf("screenresolution version %s\nLicensed under GPLv2\n", VERSION);
-                keepgoing = 0;
+   
+                break;
             } else {
-                NSLog(CFSTR("I'm sorry %s. I'm afraid I can't do that"), getlogin());
+                printf("I'm sorry %s. I'm afraid I can't do that\n", getlogin());
+                // Send help information to stderr
+                printf("Error: unable to copy current display mode\n\n");
+                printf("screenresolution version %s -- Licensed under GPLv2\n\n\n", VERSION);
+                printf("usage: screenresolution [get]  - Show the resolution of all active displays\n");
+                printf("       screenresolution [list] - Show available resolutions of all active displays\n");
+                printf("       screenresolution [skip] [display1resolution] [display2resolution]\n");
+                printf("                                - Sets display resolution and refresh rate\n");
+                printf("       screenresolution --version - Displays version information for screenresolution\n");
+                printf("       screenresolution --help    - Displays this help information\n\n");
+                printf("examples: screenresolution 800x600x32            - Sets main display to 800x600x32\n");
+                printf("          screenresolution 800x600x32 800x600x32 - Sets both displays to 800x600x32\n");
+                printf("          screenresolution skip 800x600x32       - Sets second display to 800x600x32\n\n");
                 exitcode++;
-                keepgoing = 0;
+ 
+                break;
             }
         }
         free(activeDisplays);
         activeDisplays = NULL;
     } else {
-        NSLog(CFSTR("%s"), "Incorrect command line");
-        exitcode++;
+            CGRect screenFrame = CGDisplayBounds(kCGDirectMainDisplay);  
+            CGSize screenSize  = screenFrame.size;  
+            printf("%.0f %.0f\n", screenSize.width, screenSize.height);    
     }
     return exitcode > 0;
 }
@@ -159,9 +182,10 @@ unsigned int configureDisplay(CGDirectDisplayID display, struct config *config, 
     unsigned int returncode = 1;
     CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, NULL);
     if (allModes == NULL) {
-        NSLog(CFSTR("Error: failed trying to look up modes for display %u"), displayNum);
+        printf("Error: failed trying to look up modes for display %u\n", displayNum);
+        return 0; 
     }
-
+    
     CGDisplayModeRef newMode = NULL;
     CGDisplayModeRef possibleMode;
     size_t pw; // possible width.
@@ -186,11 +210,11 @@ unsigned int configureDisplay(CGDirectDisplayID display, struct config *config, 
     }
     CFRelease(allModes);
     if (newMode != NULL) {
-        NSLog(CFSTR("set mode on display %u to %ux%ux%u@%.0f"), displayNum, pw, ph, pd, pr);
+        printf("set mode on display %u to %lux%lux%lu@%.0f\n", displayNum, pw, ph, pd, pr);
         setDisplayToMode(display,newMode);
     } else {
-        NSLog(CFSTR("Error: mode %ux%ux%u@%f not available on display %u"), 
-                config->w, config->h, config->d, config->r, displayNum);
+        printf("Error: mode %lux%lux%lu@%f not available on display %u\n", 
+              config->w, config->h, config->d, config->r, displayNum);
         returncode = 0;
     }
     return returncode;
@@ -201,17 +225,17 @@ unsigned int setDisplayToMode(CGDirectDisplayID display, CGDisplayModeRef mode) 
     CGDisplayConfigRef config;
     rc = CGBeginDisplayConfiguration(&config);
     if (rc != kCGErrorSuccess) {
-        NSLog(CFSTR("Error: failed CGBeginDisplayConfiguration err(%u)"), rc);
+        printf("Error: failed CGBeginDisplayConfiguration err(%u)\n", rc);
         return 0;
     }
     rc = CGConfigureDisplayWithDisplayMode(config, display, mode, NULL);
     if (rc != kCGErrorSuccess) {
-        NSLog(CFSTR("Error: failed CGConfigureDisplayWithDisplayMode err(%u)"), rc);
+        printf("Error: failed CGConfigureDisplayWithDisplayMode err(%u)\n", rc);
         return 0;
     }
     rc = CGCompleteDisplayConfiguration(config, kCGConfigureForSession);
     if (rc != kCGErrorSuccess) {
-        NSLog(CFSTR("Error: failed CGCompleteDisplayConfiguration err(%u)"), rc);        
+        printf("Error: failed CGCompleteDisplayConfiguration err(%u)\n", rc);        
         return 0;
     }
     return 1;
@@ -221,15 +245,15 @@ unsigned int listCurrentMode(CGDirectDisplayID display, int displayNum) {
     unsigned int returncode = 1;
     CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(display);
     if (currentMode == NULL) {
-        NSLog(CFSTR("%s"), "Error: unable to copy current display mode");
-        returncode = 0;
+        printf("Error: unable to copy current display mode\n");
+        return 0;
     }
-    NSLog(CFSTR("Display %d: %ux%ux%u@%.0f"),
-           displayNum,
-           CGDisplayModeGetWidth(currentMode),
-           CGDisplayModeGetHeight(currentMode),
-           bitDepth(currentMode),
-           CGDisplayModeGetRefreshRate(currentMode));
+    printf("Display %d: %lux%lux%lu@%.0f\n",
+          displayNum,
+          CGDisplayModeGetWidth(currentMode),
+          CGDisplayModeGetHeight(currentMode),
+          bitDepth(currentMode),
+          CGDisplayModeGetRefreshRate(currentMode));
     CGDisplayModeRelease(currentMode);
     return returncode;
 }
@@ -239,11 +263,12 @@ unsigned int listAvailableModes(CGDirectDisplayID display, int displayNum) {
     int i;
     CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, NULL);
     if (allModes == NULL) {
-        returncode = 0;
+        printf("Error: unable to copy all display mode\n");
+        return  0;
     }
 #ifndef LIST_DEBUG
     printf("Available Modes on Display %d\n", displayNum);
-
+    
 #endif
     CGDisplayModeRef mode;
     for (i = 0; i < CFArrayGetCount(allModes) && returncode; i++) {
@@ -257,43 +282,43 @@ unsigned int listAvailableModes(CGDirectDisplayID display, int displayNum) {
         }
         char modestr [50];
         sprintf(modestr, "%lux%lux%lu@%.0f",
-               CGDisplayModeGetWidth(mode),
-               CGDisplayModeGetHeight(mode),
-               bitDepth(mode),
-               CGDisplayModeGetRefreshRate(mode));
+                CGDisplayModeGetWidth(mode),
+                CGDisplayModeGetHeight(mode),
+                bitDepth(mode),
+                CGDisplayModeGetRefreshRate(mode));
         printf("%-20s ", modestr);
         if (i % MODES_PER_LINE == MODES_PER_LINE - 1) {
             printf("\n");
         }
 #else
         uint32_t ioflags = CGDisplayModeGetIOFlags(mode);
-        printf("display: %d %4lux%4lux%2lu@%.0f usable:%u ioflags:%4x valid:%u safe:%u default:%u",
-                displayNum,
-                CGDisplayModeGetWidth(mode),
-                CGDisplayModeGetHeight(mode),
-                bitDepth(mode),
-                CGDisplayModeGetRefreshRate(mode),
-                CGDisplayModeIsUsableForDesktopGUI(mode),
-                ioflags,
-                ioflags & kDisplayModeValidFlag ?1:0,
-                ioflags & kDisplayModeSafeFlag ?1:0,
-                ioflags & kDisplayModeDefaultFlag ?1:0 );
-        printf(" safety:%u alwaysshow:%u nevershow:%u notresize:%u requirepan:%u int:%u simul:%u",
-                ioflags & kDisplayModeSafetyFlags ?1:0,
-                ioflags & kDisplayModeAlwaysShowFlag ?1:0,
-                ioflags & kDisplayModeNeverShowFlag ?1:0,
-                ioflags & kDisplayModeNotResizeFlag ?1:0,
-                ioflags & kDisplayModeRequiresPanFlag ?1:0,
-                ioflags & kDisplayModeInterlacedFlag ?1:0,
-                ioflags & kDisplayModeSimulscanFlag ?1:0 );
-        printf(" builtin:%u notpreset:%u stretched:%u notgfxqual:%u valagnstdisp:%u tv:%u vldmirror:%u\n",
-                ioflags & kDisplayModeBuiltInFlag ?1:0,
-                ioflags & kDisplayModeNotPresetFlag ?1:0,
-                ioflags & kDisplayModeStretchedFlag ?1:0,
-                ioflags & kDisplayModeNotGraphicsQualityFlag ?1:0,
-                ioflags & kDisplayModeValidateAgainstDisplay ?1:0,
-                ioflags & kDisplayModeTelevisionFlag ?1:0,
-                ioflags & kDisplayModeValidForMirroringFlag ?1:0 );
+        printf("display: %d %4lux%4lux%2lu@%.0f usable:%u ioflags:%4x valid:%u safe:%u default:%u\n",
+               displayNum,
+               CGDisplayModeGetWidth(mode),
+               CGDisplayModeGetHeight(mode),
+               bitDepth(mode),
+               CGDisplayModeGetRefreshRate(mode),
+               CGDisplayModeIsUsableForDesktopGUI(mode),
+               ioflags,
+               ioflags & kDisplayModeValidFlag ?1:0,
+               ioflags & kDisplayModeSafeFlag ?1:0,
+               ioflags & kDisplayModeDefaultFlag ?1:0 );
+        printf("safety:%u alwaysshow:%u nevershow:%u notresize:%u requirepan:%u int:%u simul:%u\n",
+               ioflags & kDisplayModeSafetyFlags ?1:0,
+               ioflags & kDisplayModeAlwaysShowFlag ?1:0,
+               ioflags & kDisplayModeNeverShowFlag ?1:0,
+               ioflags & kDisplayModeNotResizeFlag ?1:0,
+               ioflags & kDisplayModeRequiresPanFlag ?1:0,
+               ioflags & kDisplayModeInterlacedFlag ?1:0,
+               ioflags & kDisplayModeSimulscanFlag ?1:0 );
+        printf("builtin:%u notpreset:%u stretched:%u notgfxqual:%u valagnstdisp:%u tv:%u vldmirror:%u\n",
+               ioflags & kDisplayModeBuiltInFlag ?1:0,
+               ioflags & kDisplayModeNotPresetFlag ?1:0,
+               ioflags & kDisplayModeStretchedFlag ?1:0,
+               ioflags & kDisplayModeNotGraphicsQualityFlag ?1:0,
+               ioflags & kDisplayModeValidateAgainstDisplay ?1:0,
+               ioflags & kDisplayModeTelevisionFlag ?1:0,
+               ioflags & kDisplayModeValidForMirroringFlag ?1:0 );
 #endif
     }
     CFRelease(allModes);
@@ -309,7 +334,7 @@ unsigned int parseStringConfig(const char *string, struct config *out) {
     int numConverted = sscanf(string, "%lux%lux%lu@%lf", &w, &h, &d, &r);
     if (numConverted != 4) {
         rc = 0;
-        NSLog(CFSTR("Error: the mode '%s' couldn't be parsed"), string);
+        printf("Error: the mode '%s' couldn't be parsed", string);
     } else {
         out->w = w;
         out->h = h;
